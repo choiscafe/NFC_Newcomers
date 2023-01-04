5709d34f8f0ac4a52aeca56ad3504100710b55df
choiscafe <choiscafe@gmail.com>
2023-01-04 17:46:32 -0500

    typeit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import TypeIt from 'typeit-react';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://images.squarespace-cdn.com/content/v1/5b0efe015cfd793504d5d5ab/1534993905752-5LYTBKUJFBNKH06TCOYR/NFC_HORIZONTAL_LOCKUP_white_lg.png?format=1500w" className="App-logo" alt="logo" />
+        <h1>
+          <TypeIt>NEWCOMERS REGISTRATION FORM</TypeIt>
+        </h1>
+
+        <h3>
+          <TypeIt
+            getBeforeInit={(instance) => {
+              instance
+                .type("Hello, New family members. Welcome.")
+                .pause(750)
+                .delete(8, { delay: 1000 })
+                .type("Shalom!")
+                .go();
+
+              // Remember to return it!
+              return instance;
+            }}
+          />
+        </h3><br></br>
         <p>
-          NEWCOMERS REGISTRATION FORM
+          We welcome you in the name of the Lord.<br></br> If you are new to New Frontier Church (NFC), please fill out and submit the form below.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* </section>
+        </div> */}
       </header>
     </div>
   );
