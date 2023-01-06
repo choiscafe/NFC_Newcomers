333b7c1f7f42aacbc5bfe1ed1e871855001180b8
choiscafe <choiscafe@gmail.com>
2023-01-06 11:08:00 -0500

    dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -5,108 +5,108 @@ import { useState } from "react"
 
 function App() {
 
-  const [values, setValues] = useState({
-    name: "",
-    gender: "",
-    dateOfBirth: "",
-    community: "",
-    contactNumber: "",
-    emailAddress: "",
-    kakaoId: "",
-    boroughsOfNewYorkCity: "",
-    baptism: ""
-  })
+  // const [values, setValues] = useState({
+  //   name: "",
+  //   gender: "",
+  //   dateOfBirth: "",
+  //   community: "",
+  //   contactNumber: "",
+  //   emailAddress: "",
+  //   kakaoId: "",
+  //   boroughsOfNewYorkCity: "",
+  //   baptism: ""
+  // })
 
-  const inputs = [
-    {
-      id: 1,
-      name: "name",
-      type: "text",
-      placeholder: "Name",
-      errorMessage: "Valid name must be entered",
-      label: "Name",
-      pattern: "^[a-zA-Z_ ]*$",
-      required: true
-    },
-    {
-      id: 2,
-      name: "gender",
-      type: "text",
-      placeholder: "Gender",
-      errorMessage: "Genders must be entered",
-      label: "Gender",
-      pattern: "^[a-zA-Z_ ]*$",
-      required: true
-    },
-    {
-      id: 3,
-      name: "dateOfBirth",
-      type: "date",
-      placeholder: "Date of Birth",
-      label: "Date of Birth",
-    },
-    {
-      id: 4,
-      name: "community",
-      type: "text",
-      placeholder: "Community",
-      errorMessage: "Community must be entered",
-      label: "Community",
-      required: true
-    },
-    {
-      id: 5,
-      name: "contactNumber",
-      type: "text",
-      placeholder: "Contact Number",
-      errorMessage: "Please provide a valid number",
-      label: "Contact Number",
-      pattern: "^[0-9]{10,14}$",
-      required: true
-    },
-    {
-      id: 6,
-      name: "emailAddress",
-      type: "email",
-      placeholder: "Email Address",
-      errorMessage: " It should be a valid email address",
-      label: "Email Address",
-      required: true
-    },
-    {
-      id: 7,
-      name: "kakaoId",
-      type: "text",
-      placeholder: "Kakao ID",
-      label: "Kakao ID",
-    },
-    {
-      id: 8,
-      name: "boroughsOfNewYorkCity",
-      type: "text",
-      placeholder: "Boroughs of New York City",
-      label: "Boroughs of New York City",
-      required: true
-    },
-    {
-      id: 9,
-      name: "baptism",
-      type: "text",
-      placeholder: "Baptism",
-      errorMessage: "Please enter if you have received Baptism",
-      label: "Baptism",
-      required: true
-    },
-  ]
+  // const inputs = [
+  //   {
+  //     id: 1,
+  //     name: "name",
+  //     type: "text",
+  //     placeholder: "Name",
+  //     errorMessage: "Valid name must be entered",
+  //     label: "Name",
+  //     pattern: "^[a-zA-Z_ ]*$",
+  //     required: true
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "gender",
+  //     type: "text",
+  //     placeholder: "Gender",
+  //     errorMessage: "Genders must be entered",
+  //     label: "Gender",
+  //     pattern: "^[a-zA-Z_ ]*$",
+  //     required: true
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "dateOfBirth",
+  //     type: "date",
+  //     placeholder: "Date of Birth",
+  //     label: "Date of Birth",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "community",
+  //     type: "text",
+  //     placeholder: "Community",
+  //     errorMessage: "Community must be entered",
+  //     label: "Community",
+  //     required: true
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "contactNumber",
+  //     type: "text",
+  //     placeholder: "Contact Number",
+  //     errorMessage: "Please provide a valid number",
+  //     label: "Contact Number",
+  //     pattern: "^[0-9]{10,14}$",
+  //     required: true
+  //   },
+  //   {
+  //     id: 6,
+  //     name: "emailAddress",
+  //     type: "email",
+  //     placeholder: "Email Address",
+  //     errorMessage: " It should be a valid email address",
+  //     label: "Email Address",
+  //     required: true
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "kakaoId",
+  //     type: "text",
+  //     placeholder: "Kakao ID",
+  //     label: "Kakao ID",
+  //   },
+  //   {
+  //     id: 8,
+  //     name: "boroughsOfNewYorkCity",
+  //     type: "text",
+  //     placeholder: "Boroughs of New York City",
+  //     label: "Boroughs of New York City",
+  //     required: true
+  //   },
+  //   {
+  //     id: 9,
+  //     name: "baptism",
+  //     type: "text",
+  //     placeholder: "Baptism",
+  //     errorMessage: "Please enter if you have received Baptism",
+  //     label: "Baptism",
+  //     required: true
+  //   },
+  // ]
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  // }
 
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value })
-  }
-  console.log(values)
+  // const onChange = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value })
+  // }
+  // console.log(values)
 
   return (
     <div className="App">
@@ -142,7 +142,8 @@ function App() {
         <p>
           If you are new to New Frontier Church (NFC),<br></br> please fill out and submit the form below.
         </p>
-        <form onSubmit={handleSubmit}>
+        <FormInput />
+        {/* <form onSubmit={handleSubmit}>
 
 
           {inputs.map((input) => (
@@ -155,7 +156,7 @@ function App() {
           ))}
 
           <button>Submit</button>
-        </form>
+        </form> */}
       </header>
     </div>
   );
